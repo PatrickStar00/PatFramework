5f9e62c431454f6fab1150a0f5ddcb988a4c8d1e
unknown <zhupeiji1993@126.com>
2024-04-29 20:51:22 +0800

    update

---
+++ Assets/Scripts/Framework/UI/StackEx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UI
+{
+    public class StackEx<T> : IEnumerable<T>
+    {
+        LinkedList<T> list = new LinkedList<T>();
+
+        public int Count { get { return list.Count; } }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public T Peek()
+        {
+            if (list.Last == null)
+            {
+                ILog.Warn("stack is empty");
+                return default(T);
+            }
+
+            return list.Last.Value;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uiid"></param>
+        public void Push(T uiid)
+        {
+            list.AddLast(uiid);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Pop()
+        {
+            if (list.Last != null)
+            {
+                list.RemoveLast();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Exists(T value)
+        {
+            return list.Find(value) != null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v"></param>
+        public void PullOut(T v)
+        {
+            list.Remove(v);
+        }
+
+        public bool GetNext(T v , out T next)
+        {
+            next = default(T);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list.ElementAt(i).Equals(v))
+                {
+                    if (i+1 < list.Count)
+                    {
+                        next = list.ElementAt(i + 1);
+                        return true;
+                    }
+                }
+            }            
+            return false;
+        }
+
+#if UNITY_EDITOR
+        public void PrintStack()
+        {
+            string stack = "[";
+            foreach(var t in list)
+            {
+                stack += t.ToString() + ",";
+            }
+            stack += "]";
+            UnityEngine.Debug.LogFormat("===== ui stack : {0}", stack);
+        }
+#endif
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                yield return list.ElementAt(i);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                yield return list.ElementAt(i);
+            }
+        }
+    }
+}
--- Assets/Scripts/Framework/UI/UIStack.cs
+++ Assets/Scripts/Framework/UI/UIStack.cs
@@ -16,7 +16,7 @@ namespace UI
     public class UIStack
     {
         public const int SORTING_STEP = 40;
-        //private int m_locker = 0;
+
         public int Prio { get; set; }
         public UIGroup Group { get; set; }
         public UIStack(int prio, UIGroup group)
@@ -24,6 +24,45 @@ namespace UI
             Prio = prio;
             Group = group;
         }
+
+        #region fields
+        // 所有注册的UI
+        private Dictionary<int, UIBase> m_uis = new Dictionary<int, UIBase>();
+        // ui调用堆栈
+        private StackEx<int> m_uiStack = new StackEx<int>();
+        #endregion
+
+        // 注册UI
+        public void RegisterUI(UIBase ui)
+        {
+
+            Debug.Assert(!m_uis.ContainsKey(ui.GetID()), "重复的 UI ID : " + ui.ToString());
+            ui.OwnerStack = this;
+            m_uis.Add(ui.GetID(), ui);
+        }
+        public UIBase GetUIInstance(int uiid)
+        {
+            if (m_uis.ContainsKey(uiid))
+            {
+                return m_uis[uiid];
+            }
+            if (UISystem.Instance.FormCreater != null)
+            {
+                UIBase aUI = UISystem.Instance.FormCreater.CreateForm(uiid);
+
+                if (aUI == null)
+                {
+                    Debug.LogFormat("GetUIInstance id : {0} NOT exists!", uiid);
+                    return null;
+                }
+
+                RegisterUI(aUI);
+                return aUI;
+            }
+            Debug.LogError("plz indicate a FormCreater");
+            return null;
+        }
+
 
     }
 }
