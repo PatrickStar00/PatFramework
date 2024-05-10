98870fb2ca91d2cbe5619fbcce2a079198964e09
Patrick <zhupeiji1993@126.com>
2024-05-11 00:41:22 +0800

    update

--- Assets/Scripts/Framework/UI/UIBase.cs
+++ Assets/Scripts/Framework/UI/UIBase.cs
@@ -6,6 +6,7 @@ using System;
 using UnityEngine.UI;
 using System.Reflection;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace UI
 {
@@ -22,10 +23,12 @@ namespace UI
 
 
         public GameObject m_uiGameObject { get; set; }
+        // public AnimatorTransition AnimatorTranser { get; private set; }
+        public Canvas RootCanvas { get; private set; }
+        public CanvasGroup RootCanvasGroup { get; private set; }
 
         private bool m_isActive;
         public bool IsActive { get { return m_uiGameObject != null && m_isActive; } private set { m_isActive = value; } }
-
         public void SetActive(bool active)
         {
             if (active == m_isActive)
@@ -49,9 +52,8 @@ namespace UI
                 }
             }
         }
-        public Canvas RootCanvas { get; private set; }
-        
         public UIBase PrevUI { get; internal set; }
+        public bool Destroyed { get; internal set; }
 
         private int m_sortingOrder;
         public int SortingOrder
@@ -86,7 +88,26 @@ namespace UI
             else
             {
                 int iLockerId = UILocker.Instance.Lock(3.0f);
-                Addressables.InstantiateAsync(string.Intern(GetFramePrefabName()));
+                
+                AsyncOperationHandle<GameObject> textureHandle = Addressables.InstantiateAsync(string.Intern(GetFramePrefabName()));;
+                textureHandle.Completed += (AsyncOperationHandle<GameObject> handle) =>
+                {
+                    UILocker.Instance.Unlock(iLockerId);
+                    if (Destroyed) return;
+                    if (handle.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        OnLoadInit(handle.Result);
+                    }
+                    else
+                    {
+                        Debug.LogError("UIBase.OnLoadedSuccess , ui 资源错误:" + string.Intern(GetFramePrefabName()));
+                        if (failed != null) failed();
+                        return;
+                    }
+                };
+
+                // OnLoadInit(result.Result as GameObject);
+
                 // ResourceManager.LoadAssetAsync(string.Intern(GetFramePrefabName()), ResourceType.UI, (result) =>
                 // {
                 //     UILocker.Instance.Unlock(iLockerId);
@@ -108,6 +129,10 @@ namespace UI
             }
         }
 
+        public virtual void Init(GameObject gameObject)
+        {
+        }
+
         private void DoActive(bool isDockableRefresh = false)
         {
         }
@@ -115,6 +140,92 @@ namespace UI
         private void DoInactive(bool isDockableRefresh = false)
         {
 
+        }
+
+        private void OnLoadInit(GameObject asset)
+        {
+            if (Destroyed) return;
+            var go = asset;
+            if (go == null)
+            {
+                return;
+            }
+            // 屏幕适配
+            var canvasScaler = go.GetComponent<CanvasScaler>();
+            if (canvasScaler == null)
+            {
+                canvasScaler = go.AddComponent<CanvasScaler>();
+            }
+            canvasScaler.referenceResolution = new Vector2(1136, 640);
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+            float aspecRatio = Screen.width / (float)Screen.height;
+            canvasScaler.matchWidthOrHeight = aspecRatio < 1.7 ? 0 : 1;
+
+            m_uiGameObject = go;
+            m_uiGameObject.name = go.name;
+
+            // m_uiGameObject = UnityEngine.Object.Instantiate(go);
+            // m_uiGameObject.name = go.name;
+            // AnimatorTranser = m_uiGameObject.GetComponent<AnimatorTransition>();
+
+            var uiLayer = OwnerStack.GetUILayer();
+            var uiParentTransform = OwnerStack.Group.GetCameraTransform();
+            var camera = OwnerStack.Group.GetCamera();
+
+
+            RootCanvas = m_uiGameObject.GetComponent<Canvas>();
+            RootCanvas.renderMode = RenderMode.ScreenSpaceCamera;
+
+            RootCanvasGroup = m_uiGameObject.GetComponent<CanvasGroup>();
+            if (RootCanvasGroup == null)
+            {
+                RootCanvasGroup = m_uiGameObject.AddComponent<CanvasGroup>();
+            }
+
+            m_uiGameObject.transform.SetParent(uiParentTransform);
+            RootCanvas.worldCamera = camera;
+
+            m_uiGameObject.transform.localScale = Vector3.one;
+            m_uiGameObject.transform.localPosition = Vector3.zero;
+
+            GameObjectUtils.SetAllChildrenLayer(m_uiGameObject, uiLayer);
+
+            Init(m_uiGameObject);
+
+
+            // RefreshMultiLanguage();
+            //     if (m_luaInitDelegate != null)
+            //     {
+            //         m_luaInitDelegate(m_uiGameObject, this);
+            //     }
+            // }
+
+            // if (UISystem.Instance.GlobalFonts != null)
+            // {
+            //     RefreshFont(UISystem.Instance.GlobalFonts);
+            // }
+
+            // RefreshSortingOrder();
+            // if (m_luaAfterInitDelegate != null)
+            //     m_luaAfterInitDelegate();
+            // else AfterInit();
+
+            // Inited = true;
+            // IsDelayDestroyMe = false;
+
+            // ShowAllEffects(false);
+
+            // if (m_isActive)
+            // {
+            //     Debug.LogFormat("set active true {0}", GetFramePrefabName());
+            //     DoActive();
+            // }
+            // else
+            // {
+            //     Debug.LogFormat("set active false {0}", GetFramePrefabName());
+            //     DoInactive();
+            // }
         }
 
     }
---
+++ Assets/Scripts/Framework/Utils.cs
@@ -0,0 +1,117 @@
+﻿
+//using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+    public struct ModifyWatchDog<T> where T : System.IComparable<T>
+    {
+        private T v;
+        private bool m_inited;
+
+        public void Init(T val)
+        {
+            v = val;
+            m_inited = true;
+        }
+
+        public bool Check(T val , T defaultValue)
+        {
+            if (!m_inited)
+            {
+                Init(defaultValue);
+            }
+            if (v.CompareTo(val) == 0)
+            {
+                return false;
+            }
+            else
+            {
+                v = val;
+                return true;
+            }
+
+        }
+    }
+public class GameObjectUtils
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    private static List<GameObject> m_vDontDestroyOnLoadList = new List<GameObject>();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="o"></param>
+    public static void DontDestroyOnLoad(UnityEngine.Object o)
+    {
+        if (null == o)
+        {
+            return;
+        }
+        DontDestroyOnLoad(o as MonoBehaviour);
+        DontDestroyOnLoad(o as GameObject);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="o"></param>
+    public static void DontDestroyOnLoad(MonoBehaviour o)
+    {
+        if (null == o)
+        {
+            return;
+        }
+        DontDestroyOnLoad(o.gameObject);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="o"></param>
+    public static void DontDestroyOnLoad(GameObject o)
+    {
+        if (null == o)
+        {
+            return;
+        }
+        foreach (Object obj in m_vDontDestroyOnLoadList)
+        {
+            if (obj == o)
+            {
+                return;
+            }
+        }
+        m_vDontDestroyOnLoadList.Add(o);
+        Object.DontDestroyOnLoad(o);
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="layerName"></param>
+    public static void SetAllChildrenLayer(GameObject go, string layerName)
+    {
+        Transform[] trans = go.GetComponentsInChildren<Transform>(true);
+        int layer = LayerMask.NameToLayer(layerName);
+        for (int i = 0; i < trans.Length; ++i)
+        {
+            trans[i].gameObject.layer = layer;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="go"></param>
+    /// <param name="layer"></param>
+    public static void SetAllChildrenLayer(GameObject go, int layer)
+    {
+        Transform[] trans = go.GetComponentsInChildren<Transform>(true);
+        //int layer = LayerMask.NameToLayer(layerName);
+        for (int i = 0; i < trans.Length; ++i)
+        {
+            trans[i].gameObject.layer = layer;
+        }
+    }
+
+
+}
